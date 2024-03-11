3690ff5c95acb1f397abd6aa952111180429e295
mshui163 <2980237857@qq.com>
2024-03-11 22:02:48 +0800

    新增了一些脚本

---
+++ src/UnityFireSafetyProject/Assets/Scripts/QframeworkTest.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using QFramework;
+public class QframeworkTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        LogKit.I("打印一些信息...Qframework安装成功");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
