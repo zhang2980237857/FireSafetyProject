54eca162c155174f92a38aeacd7c88ebc529b29d
chengong666 <3094965654@qq.com>
2024-03-12 20:31:18 +0800

    修改了代码

--- src/UnityFireSafetyProject/Assets/Scripts/QframeworkTest.cs
+++ src/UnityFireSafetyProject/Assets/Scripts/QframeworkTest.cs
@@ -7,7 +7,7 @@ public class QframeworkTest : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        LogKit.I("打印一些信息...Qframework安装成功");
+        LogKit.I("打印一些信息...Qframework安装成功辅导费 ");
     }
 
     // Update is called once per frame
