d998b409102f7b87a95bdb7833da6fdf7c414087
mshui163 <2980237857@qq.com>
2024-03-20 11:12:33 +0800

    高光与点击交互

--- src/UnityFireSafetyProject/Assets/Scripts/Game/ViewContorller/VisionStick.cs
+++ src/UnityFireSafetyProject/Assets/Scripts/Game/ViewContorller/VisionStick.cs
@@ -3,29 +3,40 @@ using QFramework;
 
 namespace QFramework.UnityFireSafetyProject
 {
-	public partial class VisionStick : ViewController
+
+    public partial class VisionStick : ViewController
 	{
+        Collider other1;
 		void Start()
 		{
 			
 		}
+
+        private void Update()
+        {
+            if (other1 != null&& Input.GetMouseButtonDown(0))
+            {
+                Debug.Log("鼠标点击...");
+            }
+        }
         private void OnTriggerEnter(Collider other)
         {
             if (other.tag == "InteractiveObj") //如何检测的是可交互物体就让它高光
             {
+                other1 = other; 
                 //改为脚本的显示跟取消
                 other.GetComponent<Outline>().enabled = true;
                 other.GetComponent<Outline>().OutlineMode = Outline.Mode.OutlineVisible;
-                Debug.Log("物体进入触发");
+                
             }
         }
         private void OnTriggerExit(Collider other)
         {
+            other1 = null;
             if (other.tag == "InteractiveObj") //如何检测的是可交互物体就让它高光
             {
                 other.GetComponent<Outline>().enabled = false;
                 other.GetComponent<Outline>().OutlineMode = Outline.Mode.OutlineHidden;
-                Debug.Log("物体移开触发");
             }
         }
     }
